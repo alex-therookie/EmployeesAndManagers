1f8761f3ee1c272304e556645f1296d89ab43287
jxnprince <68714241+jxnprince@users.noreply.github.com>
2020-12-02 17:32:10 -0700

    Almost done....

--- employee.js
+++ employee.js
@@ -1,23 +1,15 @@
 class Employee {
     constructor(name, title, salary, boss) {
-        this.name = name;
-        this.title = title;
-        this.salary = salary;
-        this.boss = boss;
+      this.name = name;
+      this.title = title;
+      this.salary = salary;
+      this.boss = boss;
     }
+  bonus(multiplier) { 
+    return this.salary * multiplier
+  }
 }
 
-const employee1 = new Employee('alex', 'programmer', 99000, 'alissa');
-const employee2 = new Employee('jackson', 'programmer', 99000, 'alissa');
-const employee3 = new Employee('fred', 'programmer', 99000, 'javier');
-
-// console.log(employee1);
-// console.log(employee2);
-// console.log(employee3);
-
 module.exports = {
-    Employee,
-    employee1,
-    employee2,
-    employee3
+    Employee
 }
--- manager.js
+++ manager.js
@@ -1,25 +1,38 @@
-const { Employee,
-    employee1,
-    employee2,
-    employee3 } = require("./employee.js");
+const { Employee
+      } = require("./employee.js");
 
 class Manager extends Employee {
-    constructor(name, title, salary, team) {
-        super(name, title, salary)
-        this.team = [];
+    constructor(name, title, salary, boss, team) {
+        super(name, title, salary, boss)
+      this.team = []
     }
-
-    getTeam() {
-        if (Employee.boss === this.name) {
-            this.team.push(Employee.name);
-        }
-    }
-
+  addTeamMember(name) { 
+      this.team.push(name)
+  }
+  totalSubSalary() { 
+    let totalEmployeeSalary = this.team.reduce((acc, el) => {
+        return acc.salary += el.salary
+      })
+    return totalEmployeeSalary + this.salary;
+  }
+  
+  bonus(multiplier) { 
+    return this.totalSubSalary() + this.salary * multiplier;
+  }
+  
 }
+const employee1 = new Employee("alex", "programmer", 99000, "alissa");
+const employee2 = new Employee("jackson", "programmer", 99000, "alissa");
 
-const manager1 = new Manager('alissa', 'manager', 10);
+const manager1 = new Manager("alissa", "manager", 99010, "javier");
+manager1.addTeamMember(employee1)
+manager1.addTeamMember(employee2)
+const manager2 = new Manager('javier', 'bigBoss', 99020);
+manager2.addTeamMember(manager1)
 
-console.log(manager1);
-console.log(employee1);
-console.log(employee2);
-console.log(employee3);
+
+// console.log(manager1.team)
+console.log(manager2.totalSubSalary());
+// console.log(manager2.team);
+// console.log(manager2.getTeam());
+// console.log(manager2.team);
